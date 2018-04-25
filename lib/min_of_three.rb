14298468db7bb6e475326252dd6883e6d5cb49ff
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:34:03 +0200

    Dokumenterade min_of_three

--- lib/min_of_three.rb
+++ lib/min_of_three.rb
@@ -1,3 +1,15 @@
+# Public: Takes three numbers and checks wich one is the smallest.
+#
+# num1 - One integer to compare 
+# num2 - One integer to compare
+# num3 - One integer to compare
+#
+# Examples
+#
+#  min_of_three(1,2,3)
+#  # => 1
+#
+# Returns the smalles number
 def min_of_three(num1,num2,num3)
     if num1 == num2
         if num3 < num2
