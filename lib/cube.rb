51eda233c7badb833e492e64918f0b2c0d7cd977
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:49:01 +0200

    Dokumenterade cube

--- lib/cube.rb
+++ lib/cube.rb
@@ -1,3 +1,13 @@
+#Takes a number and takes it times itself two times
+#
+# number - The integer to be cubed
+#
+# Examples
+#
+#  cube(5)
+#  # => 125
+#
+# Return number cubed
 def cube(number)
     return number*number*number
 end
