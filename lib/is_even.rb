ea9e80ba386715383fd84b9c1e28e9189c36d1d7
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:58:53 +0200

    Dokumenterade is_even

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,15 @@
+# Public: checks if the number is even or not.
+#
+# number - The integer to be checked
+#
+# Examples
+#
+#  is_even(4)
+#  # => true
+#  is_even(5)
+#  # => false
+#
+# Returns true of false depending on if the nuber is even or not
 def is_even(number)
     if number%2 > 0
         return false
