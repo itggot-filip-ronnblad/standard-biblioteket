ec4bd46da4d9847e15ce6d6147a1ecc29e465c4d
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:41:10 +0200

    Dokumenterade max_of_two

--- lib/max_of_two.rb
+++ lib/max_of_two.rb
@@ -1,3 +1,14 @@
+# Public: Takes two number check which one is the biggest
+#
+# num1 - One integer to compare
+# num2 - One integer to compare
+#
+# Examples
+#
+#  max_of_two(1,2)
+#  # => 2
+#
+# Returns the largest number
 def max_of_two(num1,num2)
     if num1>=num2
         return num1
