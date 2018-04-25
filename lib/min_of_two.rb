9085fd248f627060e24e954af888b9c4d8d74ff6
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:31:31 +0200

    Dokumenterade min_of_two

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,14 @@
+# Public: Takes two numbers and checks wich one is the smallest.
+#
+# num1 - One integer to compare 
+# num2 - One integer to compare
+#
+# Examples
+#
+#  min_of_two(1,2)
+#  # => 1
+#
+# Returns the smallest number 
 def min_of_two(num1,num2)
     if num1 == num2
         return "equal"
