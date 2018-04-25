6bb232e38bc17ef2dd61e854b72fd30653ebc606
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:42:42 +0200

    Dokumenterade max_of_three

--- lib/max_of_three.rb
+++ lib/max_of_three.rb
@@ -1,5 +1,17 @@
 require_relative 'lib.rb'
 
+# Public: Takes three number check which one is the biggest
+#
+# num1 - One integer to compare
+# num2 - One integer to compare
+# num3 - One integer to compare
+#
+# Examples
+#
+#  max_of_three(1,2,3)
+#  # => 3
+#
+# Returns the largest number
 def max_of_three(num1,num2,num3)
     if max_of_two(num1,num2) <= num3
         return num3
