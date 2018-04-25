c3ff43e8f7ec78782862675c394ca78695ef8122
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:12:50 +0200

    Dokumenterade between

--- lib/between.rb
+++ lib/between.rb
@@ -1,3 +1,15 @@
+# Public: Takes three numbers and checks if a number is between the others
+#
+# num1 - One integer to be compared with others.
+# num2 - One integer to be compared with others.  
+# num3 - One integer to be compared with others.
+#
+# Examples
+#
+#  between(1,2,3)
+#  # => true
+#
+# Returns wheter the result is true or false 
 def between(num1,num2,num3)
     if num2 > num3
         if num1>= num3 && num1<= num2
