83c1dfb644218fdaff2b6c8d59debd5a9dc14480
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:28:19 +0200

    Dokumenterade between_strict

--- lib/between_strict.rb
+++ lib/between_strict.rb
@@ -1,4 +1,17 @@
-# Public
+# Public: Takes three numbers and checks if the first one is strictly between the others.
+#
+# num1 - The integer to check
+# num2 - One integer as a limit
+# num3 - One integer as a limit
+#
+# Example
+#
+#  between_strict(2,1,4)
+#  # => true
+#  between_strict(2,2,4)
+#  # => false
+#
+# Returns Wheter or not num1 is strictly between the two others.
 def between_strict(num1,num2,num3)
     if num2 > num3
         if num1>num3 && num1<num2
