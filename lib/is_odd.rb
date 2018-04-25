4850f7976950548c8e8b158b53039c870fd4bd72
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:01:20 +0200

    Dokumenterade is_odd

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,15 @@
+# Public: Checks wheter a number is odd or not
+#
+# number - The integer to be checked
+#
+# Examples
+#
+#  is_odd(4)
+#  # => false
+#  is_odd(5)
+#  # => true
+#
+# Returns true or false depending on if the number is odd or not.
 def is_odd(number)
     if number%2 > 0
         return true
