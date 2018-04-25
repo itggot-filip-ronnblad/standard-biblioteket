4011e195f39e491f567143d54986b54aef925703
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:41:32 +0200

    Dokumenterade previous_number

--- lib/previous_number.rb
+++ lib/previous_number.rb
@@ -1,3 +1,13 @@
+# Public: Takes a number and returns the number before.
+#
+# number - The integer to be reduced
+#
+# Examples
+#
+#  previous_number(5)
+#  # => 4
+#
+# Returns number -1
 def previous_number(number)
     return number-1
 end
