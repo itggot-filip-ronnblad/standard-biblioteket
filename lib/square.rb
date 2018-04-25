2939b554d66a47e4d63b04812c466c33a913ce91
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:45:27 +0200

    Dokumenterade square

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,13 @@
+# Public: Take a number and take it times itself.
+#
+# number - The integer to be squared.
+#
+# Examples
+#
+#  square(5)
+#  # => 25
+#
+#Returns the number times itself.
 def square(number)
     return number*number
 end
