182a3dcb49c2a3c05c8ada5ff03d9825731b8b1c
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:55:04 +0200

    Dokumenterade is_negative

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,15 @@
+# Public: Takes a number and checks if it is negative
+#
+# number - The integer or float that will be checed
+#
+# Examples
+#
+#  is_negative(-5)
+#  # => true
+#  is_negative(5)
+#  # => false
+#
+# Returns true or false depending on if the number is negative or not.
 def is_negative(number)
     if number < 0
         return true
