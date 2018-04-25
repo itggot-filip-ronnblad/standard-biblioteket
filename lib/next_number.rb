778e53afe53b406c16ae2a515e319384f28b4354
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 13:38:46 +0200

    Dokumenterade next_number

--- lib/next_number.rb
+++ lib/next_number.rb
@@ -1,3 +1,13 @@
+# Public: Take a number and returns the number after.
+#
+# number - The integer to be raised.
+#
+# Examples
+#
+#  next_number(5)
+#  # => 6
+#
+# Returns number + 1 
 def next_number(number)
     return number+1
 end
