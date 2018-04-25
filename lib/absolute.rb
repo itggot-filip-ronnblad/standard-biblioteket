0b38a6cb539e518813dfba3479a56b43aaf070ed
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-25 14:06:13 +0200

    Revert "Dokumenterade absolute"
    This reverts commit 12ed0162f6a560480587a009f2d1cc2ee7865a40.

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,20 +1,8 @@
 require_relative 'lib.rb'
 
-# Public: Takes a number and returns the positive version of it.
-#
-# number - The integer or float to change to absolute.
-#
-# Examples
-#
-#  absolute(-5)
-#  # => 5
-#  absolute(5)
-#  # => 5
-#
-# Returns the absolute version of number. 
 def absolute(number)
-    if is_negative(number) == true
-        return number * -1
+    if is_even(number) == true
+        return number
     end
-    return number
+    return number * -1
 end
