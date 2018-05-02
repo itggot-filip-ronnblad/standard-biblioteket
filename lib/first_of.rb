41877b0675bdc32eec6fb511561bea3fc88c7a7e
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-05-02 13:21:44 +0200

    Gjorde first of och last of

---
+++ lib/first_of.rb
@@ -0,0 +1,14 @@
+
+# Public: Takes an array and returns the first index
+#
+# arr - The array
+#
+# Examples
+#
+#  first_of([1,2,3])
+#  first_of([1,2,3]) => 1
+#
+# Returns the first index in the array
+def first_of(arr)
+    return arr[0]
+end
---
+++ lib/last_of.rb
@@ -0,0 +1,14 @@
+
+# Public: Takes an array and returns the last index
+#
+# arr - The array
+#
+# Examples
+#
+#  last_of([1,2,3])
+#  last_of([1,2,3]) => 3
+#
+# Returns the last index in arr
+def last_of(arr)
+    return arr[arr.length-1]
+end
